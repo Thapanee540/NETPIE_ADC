539d4076d2c35e5cacb8ace897f38c2a64321a74
nguang <nongnguang@gmail.com>
2018-09-10 21:18:29 +0700

    edit index

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <script src="/microgear-html5/build/microgear.js"></script>
 
-<script src="js/raphael.2.1.0.min.js"></script>
-<script src="js/justgage.1.0.1.min.js"></script>
+<script src="/js/raphael.2.1.0.min.js"></script>
+<script src="/js/justgage.1.0.1.min.js"></script>
 
 <script src="https://netpie.io/microgear.js"></script>
 
