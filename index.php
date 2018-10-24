b12856b9c8d13f5a2eba56868eda19a9dae4712e
nguang <nongnguang@gmail.com>
2018-10-24 17:47:00 +0700

    edit v3

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <script src="microgear-html5/build/microgear.js"></script>
 
 
-<script src="/js/raphael.2.1.0.min.js"></script>
-<script src="/js/justgage.1.0.1.min.js"></script>
+<script src="js/raphael.2.1.0.min.js"></script>
+<script src="js/justgage.1.0.1.min.js"></script>
 
 <!-- <script src="https://netpie.io/microgear.js"></script> -->
 <link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree" rel="stylesheet">
